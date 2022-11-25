8ca8eab0835da0fd64d5cced10c2f9268b652cd3
MD Taseen Khan <85288261+MohammadTaseenKhan@users.noreply.github.com>
2022-11-26 00:14:48 +0600

    update

--- theme.config.js
+++ theme.config.js
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Kali Solve: Solve Your Kali Linux Problem" />
+      <meta name="og:description" content="Kali Solve: Solve Your Kali Linux Problem" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://www.kalisolve.ml/og.png" />
+      <meta name="twitter:site:domain" content="www.kalisolve.ml" />
+      <meta name="twitter:url" content="https://www.kalisolve.ml/" />
+      <meta name="og:title" content="Kali Solve: Solve Your Kali Linux Problem" />
+      <meta name="og:image" content="https://www.kalisolve.ml/og.png" />
+      <meta name="apple-mobile-web-app-title" content="Kali Solve" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © Kali Solve.</>,
   unstable_faviconGlyph: '👋',
 }
