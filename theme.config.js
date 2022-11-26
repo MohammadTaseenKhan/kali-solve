4c169ff38c003350638c6ab979f8271dbb79894e
MD Taseen Khan <85288261+MohammadTaseenKhan@users.noreply.github.com>
2022-11-26 13:23:39 +0600

    u

--- theme.config.js
+++ theme.config.js
@@ -12,8 +12,8 @@ export default {
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#000000" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
       <meta name="description" content="Kali Solve: Solve Your Kali Linux Problem" />
