2663781266fa97700af616fd62aae9e7e959c9ab
MD Taseen Khan <85288261+MohammadTaseenKhan@users.noreply.github.com>
2022-12-01 14:19:02 +0600

    u

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
+
+
+<script src="https://giscus.app/client.js"
+        data-repo="MohammadTaseenKhan/kali-solve"
+        data-repo-id="R_kgDOIgNbNg"
+        data-category="[ENTER CATEGORY NAME HERE]"
+        data-category-id="[ENTER CATEGORY ID HERE]"
+        data-mapping="url"
+        data-strict="1"
+        data-reactions-enabled="1"
+        data-emit-metadata="1"
+        data-input-position="top"
+        data-theme="dark_high_contrast"
+        data-lang="en"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
+</script>
+
+
+
+
   return <Component {...pageProps} />
 }
